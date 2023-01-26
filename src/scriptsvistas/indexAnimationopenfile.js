329376340ede51d558f37d795f9e74139b088d2c
JOSEMANUEL <73414733+SANTIAGOVELASCOJOSEMANUEL@users.noreply.github.com>
2023-01-26 02:13:12 -0600

    Update indexAnimationopenfile.js
    #11

---
+++ src/scriptsvistas/indexAnimationopenfile.js
@@ -0,0 +1,56 @@
+document.getElementById("open-file").addEventListener("click",modal1);
+function modal1(){
+   
+    
+    
+    document.getElementById("Open-fixed").classList.add("modal")
+}
+
+
+document.getElementById("save-file").addEventListener("click",modal2);
+document.getElementById("sidemenu").addEventListener("click",modal2);
+document.getElementById("sub-menusActions1").addEventListener("click",modal2);
+document.getElementById("sub-menusActions2").addEventListener("click",modal2);
+document.getElementById("sub-menusActions3").addEventListener("click",modal2);
+document.getElementById("sub-menusActions4").addEventListener("click",modal2);
+document.getElementById("sub-menusActions5").addEventListener("click",modal2);
+document.getElementById("sub-menusActions6").addEventListener("click",modal2);
+document.getElementById("sub-menusActions7").addEventListener("click",modal2);
+document.getElementById("sub-menusActions8").addEventListener("click",modal2);
+document.getElementById("sub-menusActions9").addEventListener("click",modal2);
+document.getElementById("sub-menusActions10").addEventListener("click",modal2);
+document.getElementById("sub-menusActions11").addEventListener("click",modal2);
+document.getElementById("sub-menusActions12").addEventListener("click",modal2);
+document.getElementById("sub-menusActions13").addEventListener("click",modal2);
+document.getElementById("sub-menusActions14").addEventListener("click",modal2);
+document.getElementById("sub-menusActions15").addEventListener("click",modal2);
+document.getElementById("sub-menusActions16").addEventListener("click",modal2);
+document.getElementById("sub-menusActions17").addEventListener("click",modal2);
+document.getElementById("sub-menusActions18").addEventListener("click",modal2);
+document.getElementById("sub-menusActions19").addEventListener("click",modal2);
+document.getElementById("sub-menusActions20").addEventListener("click",modal2);
+document.getElementById("sub-menusActions21").addEventListener("click",modal2);
+
+
+document.getElementById("options-canvas").addEventListener("click",modal2);
+document.getElementById("sub-menusActions22").addEventListener("click",modal2);
+document.getElementById("sub-menus2").addEventListener("click",modal2);
+document.getElementById("sub-menus3").addEventListener("click",modal2);
+document.getElementById("sub-menus4").addEventListener("click",modal2);
+document.getElementById("sub-menus5").addEventListener("click",modal2);
+document.getElementById("sub-menus6").addEventListener("click",modal2);
+document.getElementById("sub-menus7").addEventListener("click",modal2);
+document.getElementById("sub-menus8").addEventListener("click",modal2);
+document.getElementById("sub-menus9").addEventListener("click",modal2);
+function modal2(){
+   
+    
+    
+    document.getElementById("Open-fixed").classList.remove("modal")
+}
+function modal3(){
+   
+    
+    
+    document.getElementById("Open-fixed").classList.remove("modal")
+}
